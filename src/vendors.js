b6be874eef3a8abb0a98be9c9ac1133e60e1070c
Blaise Laflamme <blaise@laflamme.org>
2016-12-04 21:12:16 -0500

    add vendors

---
+++ src/vendors.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// jQuery
+require('jquery');
+
+// Bootstrap
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+
+// Font Awesome
+require('font-awesome/css/font-awesome.css');
+
+// Animate
+require('animate.css/animate.css');
+
+// Highlight
+require('highlight.js');
+require('highlight.js/styles/github.css');
