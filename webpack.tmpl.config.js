4d3fffd47f2736d03218cb63d4dff03feed326c4
Blaise Laflamme <blaise@laflamme.org>
2016-12-04 21:09:34 -0500

    add webpack config

---
+++ webpack.tmpl.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+var baseDir = __dirname + '/src/templates';
+
+var config = {
+  templates: [
+    {
+      template: 'src/templates/index.ejs',
+      filename: 'index.html',
+      uglify: true,
+      context: require(path.resolve(baseDir, 'index.json')),
+      page: 'index.html'
+    }, {
+      template: 'src/templates/colors.ejs',
+      filename: 'colors.html',
+      uglify: true,
+      context: require(path.resolve(baseDir, 'colors.json')),
+      page: 'colors.html'
+    }
+  ]
+};
+
+module.exports = config;
